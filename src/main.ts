ef3f6907144af1af7ba9a3b84896d4ee3c1511c8
mohax <mohax.spb@gmail.com>
2019-08-08 23:28:53 +0300

    Add todo.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 import 'hammerjs';
 
 if (environment.production) {
@@ -11,3 +11,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// todo internationalization see https://angular.io/guide/i18n#the-app-and-its-translation-file

10bc50aef7f58f0c3bd4ea97d2be42b934e7ec3e
mohax <mohax.spb@gmail.com>
2019-08-20 20:11:05 +0300

    Better code for account form.

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {UserProvider} from '../service/auth/user.subject';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 import {User} from '../model/user';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {tap} from 'rxjs/operators';
+import {MyErrorStateMatcher} from '../utils/MyErrorStateMatcher';
+import {GpLanguageService} from '../service/GpLanguageService';
+import {GpAccountInteractor} from '../service/GpAccountInteractor';
+import {Language} from '../model/language';
 
 @Component({
   selector: 'app-account',
@@ -12,69 +15,79 @@ import {tap} from 'rxjs/operators';
 })
 export class AccountComponent implements OnInit {
 
-  userIsLoading = new BehaviorSubject<boolean>(false);
-  userObservable: Observable<User>;
-  userFromApi: User | null;
+  dataIsLoading = new BehaviorSubject<boolean>(false);
+  userFromApi: User;
 
   name: string;
+  userLanguage: Language;
 
   // @ViewChild('accountEditForm', {static: false})
   // accountEditForm: NgForm;
 
   accountEditFormGroup: FormGroup;
 
+  matcher = new MyErrorStateMatcher();
+
+  languagesListFromApi2: Language[];
+
   constructor(
     private userProvider: UserProvider,
+    private languageService: GpLanguageService,
+    private accountInteractor: GpAccountInteractor,
     private fBuilder: FormBuilder
   ) {
-    this.getUser();
   }
 
   ngOnInit() {
     console.log('ngOnInit');
-    // this.initForm();
+    // todo use combine latest with user from api
+    this.dataIsLoading.next(true);
+    this.accountInteractor.getUserAndLanguages()
+    // todo test error/refresh
+      .subscribe(
+        (userAndLanguages: [User, Language[]]) => {
+          this.userFromApi = userAndLanguages[0];
+          this.languagesListFromApi2 = userAndLanguages[1];
+
+          this.userLanguage = this.languagesListFromApi2.find(value => value.id === this.userFromApi.primaryLanguageId);
+          // this.primaryLanguage = userLanguage.langCode.toUpperCase();
+
+          this.dataIsLoading.next(false);
+
+          this.initForm(this.userFromApi, this.userLanguage);
+        }
+      );
   }
 
-  private initForm(user: User) {
+  private initForm(user: User, userLanguage: Language) {
     this.accountEditFormGroup = this.fBuilder.group({
       name: new FormControl(
-        {value: user.fullName, disabled: true},
+        {value: user.fullName, disabled: false},
         [Validators.required]
       ),
       email: new FormControl(
-        {value: undefined, disabled: false},
-        [Validators.required, Validators.email]
+        {value: user.email, disabled: true},
+        []
       ),
       primaryLanguageSelect: new FormControl(
-        {value: undefined, disabled: true},
+        {value: userLanguage, disabled: false},
         [Validators.required]
       )
     });
   }
 
   onNameChanged(name: string) {
+    console.log('onNameChanged: %s', name);
     this.name = name;
   }
 
-  onAccountEditClicked() {
-    console.log('onAccountEditClicked');
+  onPrimaryLanguageChanged(language: Language) {
+    console.log('onPrimaryLanguageChanged: %s', JSON.stringify(language));
+    this.userLanguage = language;
   }
 
-  getUser() {
-    this.userIsLoading.next(true);
-    this.userObservable = this.userProvider
-      .user
-      .pipe(
-        tap((user: User | null) => {
-          console.log('userObservable tap: ' + user);
-          this.userFromApi = user;
-          this.userIsLoading.next(false);
-          if (user != null) {
-            this.initForm(user);
-          }
-        }),
-      );
-    this.userObservable.subscribe();
+  onAccountEditClicked() {
+    console.log('onAccountEditClicked: %s, %s', this.name, this.userLanguage.langCode);
   }
 
   isNullOrEmptyOrUndefined(value) {

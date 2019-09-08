652cdee63c407360270d1c9e3e073aef51e51d8c
mohax <mohax.spb@gmail.com>
2019-09-08 20:40:55 +0300

    Start realizing articles list.

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -3,6 +3,10 @@ import {Router} from '@angular/router';
 import {AuthService} from '../service/auth/auth.service';
 import {MatBottomSheet} from '@angular/material';
 import {LoginComponent} from '../login/login.component';
+import {GpArticleService} from '../service/data/GpArticleService';
+import {BehaviorSubject} from 'rxjs';
+import {finalize} from 'rxjs/operators';
+import {NotificationService} from '../service/ui/NotificationService';
 
 @Component({
   selector: 'app-feed',
@@ -11,14 +15,19 @@ import {LoginComponent} from '../login/login.component';
 })
 export class FeedComponent implements OnInit {
 
+  progressInAction = new BehaviorSubject<boolean>(false);
+
   constructor(
     private router: Router,
     private authService: AuthService,
+    private articleService: GpArticleService,
+    private notificationService: NotificationService,
     private bottomSheet: MatBottomSheet
   ) {
   }
 
   ngOnInit() {
+    this.loadArticles();
   }
 
   onCreateArticleClicked() {
@@ -28,5 +37,20 @@ export class FeedComponent implements OnInit {
     } else {
       this.bottomSheet.open(LoginComponent, {data: {title: 'To create article you should'}});
     }
+  }
+
+  private loadArticles() {
+    this.progressInAction.next(true);
+
+    this.articleService.getPublishedArticles()
+      .pipe(
+        finalize(() => this.progressInAction.next(false))
+      )
+      .subscribe(
+        articles => {
+
+        },
+        error => this.notificationService.showError(error)
+      );
   }
 }

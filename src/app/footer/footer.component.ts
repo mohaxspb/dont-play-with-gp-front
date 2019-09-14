820d8741daaba73bf3ba7f8fb504314003bc9b9a
mohax <mohax.spb@gmail.com>
2019-09-14 21:52:12 +0300

    Add showing app version in footer component.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {version} from 'package.json';
 
 @Component({
   selector: 'app-footer',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  appVersion: string;
 
-  constructor() { }
+  constructor() {
+    this.appVersion = version;
+  }
 
   ngOnInit() {
   }

f6ea0c8485926025c38ab587d038a7e3de8eb50c
mohax <mohax.spb@gmail.com>
2019-09-29 20:26:08 +0300

    Realize loading and showing comments for article.

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CommentService} from '../service/data/CommentService';
+import {GpUserService} from '../service/auth/GpUserService';
+import {NotificationService} from '../service/ui/NotificationService';
+import {DialogService} from '../service/ui/DialogService';
+import {MatBottomSheet} from '@angular/material';
+import {BehaviorSubject, of} from 'rxjs';
+import {GpConstants} from '../GpConstants';
+import {GpComment} from '../model/data/GpComment';
+import {catchError, finalize} from 'rxjs/operators';
+import {UserProvider} from '../service/auth/UserProvider';
+import {GpUser} from '../model/auth/GpUser';
 
 @Component({
   selector: 'app-comments',
@@ -7,9 +18,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
 
-  constructor() { }
+  progressInAction = new BehaviorSubject<boolean>(false);
+  bottomProgressInAction = new BehaviorSubject<boolean>(false);
+  bottomProgressErrorOccurred = new BehaviorSubject<boolean>(false);
 
-  ngOnInit() {
+  user: GpUser | null = null;
+
+  @Input() articleId: number;
+
+  noCommentsForArticle = false;
+  comments: GpComment[] = [];
+
+  constructor(
+    private commentService: CommentService,
+    private userService: GpUserService,
+    private userProvider: UserProvider,
+    private notificationService: NotificationService,
+    private dialogsService: DialogService,
+    private bottomSheet: MatBottomSheet
+  ) {
   }
 
+  ngOnInit() {
+    this.loadInitialData();
+
+    // handle user changed
+    this.userProvider
+      .getUser()
+      .subscribe(value => {
+        // console.log('this.user !== value: %s', (this.user === null && value !== null) || (this.user !== null && value === null));
+        if ((this.user === null && value !== null) || (this.user !== null && value === null)) {
+          this.user = value;
+        }
+      });
+  }
+
+  onScroll() {
+    this.loadComments(this.comments.length);
+  }
+
+  commentsListEndReached(): boolean {
+    return this.comments.length % GpConstants.DEFAULT_LIMIT !== 0 || this.noCommentsForArticle;
+  }
+
+  private loadInitialData() {
+    this.progressInAction.next(true);
+
+    this.userService.getUser().pipe(catchError(() => of(null)))
+      .pipe(
+        // delay(1000),
+        finalize(() => this.loadComments())
+      )
+      .subscribe(
+        value => {
+          this.user = value;
+        },
+        error => this.notificationService.showError(error)
+      );
+  }
+
+  loadComments(offset: number = 0) {
+    console.log('loadComments: %s', offset);
+    if (offset === 0) {
+      this.progressInAction.next(true);
+    } else {
+      this.bottomProgressInAction.next(true);
+    }
+
+    this.commentService
+      .getCommentsForArticle(this.articleId, GpConstants.DEFAULT_LIMIT, offset)
+      .pipe(
+        // to test loading indicator
+        // delay(1000),
+        // to test error
+        // flatMap(value => {
+        //   if (offset === 4) {
+        //     throw Error('test');
+        //   } else {
+        //     return of(value);
+        //   }
+        // }),
+        finalize(() => {
+          if (offset === 0) {
+            this.progressInAction.next(false);
+          } else {
+            this.bottomProgressInAction.next(false);
+          }
+        })
+      )
+      .subscribe(
+        comments => {
+          if (offset === 0) {
+            this.comments = comments;
+            this.noCommentsForArticle = this.comments.length === 0;
+          } else {
+            this.bottomProgressErrorOccurred.next(false);
+            this.comments = this.comments.concat(comments);
+          }
+        },
+        error => {
+          this.notificationService.showError(error);
+          this.bottomProgressErrorOccurred.next(true);
+        }
+      );
+  }
 }
